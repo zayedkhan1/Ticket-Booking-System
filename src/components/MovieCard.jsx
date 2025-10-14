cd8a60e038c6ef71857ca204a0145e9a626c839a
zayed khan <zayedk458@gmail.com>
2025-10-14 15:33:58 +0600

    trailer section added

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,30 +1,3 @@
-// import React from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import { CiStar } from "react-icons/ci";
-// import { timeFormat } from '../libraries/TimeFormate';
-// const MovieCard = ({movie}) => {
-//     const navigate=useNavigate();
-//     return (
-//         <di className="flex flex-col justify-between p-3 bg-gray-800 rounded-2xl hover:-translate-y-1 transition duration-300 w-66">
-//             <img
-//             onClick={()=>{navigate(`/movies/${movie._id}`)}}
-//             src={movie.backdrop_path} alt="" className='rounded-ld h-52  w-full object-cover object-right-bottom curosor-pointer' />
-//             <p className='font-semibold mt-2 truncate'>{movie.title}</p>
-//             <p className='text-sm text-gray-400 mt-2'>{new Date(movie.release_date).getFullYear()}.{movie.genres.slice(0,2).map(genre=>genre.name).join("|")}.{timeFormat(movie.runtime)} </p>
-//             <div className=' flex items-center justify-between mt-4 pb-3' >
-//                 <button
-//                  onClick={()=>{navigate(`/movies/${movie._id}`)}}
-//                 className='px-4 py-2 text-xs bg-red-400 hover:bg-red-500 transition rounded-full font-medium cursor-pointer'>Buy Tickets</button>
-//                 <p className='flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1'>
-//                     <CiStar  className='w-4 h-4 text-red-500 fill-red-500' />
-//                     {movie.vote_average.toFixed(1)}
-//                 </p>
-//             </div>
-//         </di>
-//     );
-// };
-
-// export default MovieCard;
 
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -46,8 +19,6 @@ const MovieCard = ({ movie }) => {
                     alt={movie.title}
                     className='w-full h-48 sm:h-52 md:h-56 lg:h-60 object-cover object-center group-hover:scale-105 transition-transform duration-500'
                 />
-                {/* Overlay on hover */}
-                {/* <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-30 transition-all duration-300"></div> */}
             </div>
 
             {/* Content Section */}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import FeaturedSection from '../components/FeaturedSection';
+import TrailerSection from '../components/TrailerSection';
 
 const Home = () => {
     return (
         <div>
             <Hero></Hero>
             <FeaturedSection></FeaturedSection>
+            <TrailerSection></TrailerSection>
         </div>
     );
 };
